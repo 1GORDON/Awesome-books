92f494421f7e8b7488b23d22c782f0fb12a39030
zieeco <zieecosam@gmail.com>
2021-09-15 15:04:48 +0100

    Add the displayFunction

--- script.js
+++ script.js
@@ -1,37 +1,59 @@
 const saveToLocalStorage = (key, data) => localStorage.setItem(key, JSON.stringify(data));
 const getFromLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
 
-function codeForSingleBook(book){
-    return `<div>${book.title}</div>
+function displayBooks() {
+  let bookItems = getFromLocalStorage('bookItems');
+  if (bookItems == null) {
+    bookItems = [];
+  }
+
+  const booksCode = bookItems.map((book) => codeForSingleBook(book));
+  document.getElementById('items').innerHTML = booksCode.join('');
+  const removeButtons = document.querySelectorAll('.remove');
+
+  for (let i = 0; i < removeButtons.length; i++) {
+    removeButtons[i].addEventListener('click', (event) => {
+      const id = event.target.getAttribute('data-id');
+      bookItems = bookItems.filter((b) => b.id !== Number(id));
+      saveToLocalStorage('bookItems', bookItems);
+      displayBooks();
+    });
+  }
+}
+
+displayBooks();
+
+function codeForSingleBook(book) {
+  return `<div>${book.title}</div>
             <div>${book.author}</div>
             <button class="remove" data-id="${book.id}">Remove</button>
             <hr>`;
 }
 
-var titleInput = document.getElementById("title");
-var authorInput = document.getElementById("author");
-
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
 document
-  .getElementById("book-list")
-  .addEventListener("submit", function(event){
+  .getElementById('book-list')
+  .addEventListener('submit', (event) => {
     event.preventDefault();
-    var bookItems = getFromLocalStorage("bookItems");
-    if(bookItems == null){
-    bookItems = [];
+    let bookItems = getFromLocalStorage('bookItems');
+    if (bookItems == null) {
+      bookItems = [];
     }
-    var title = titleInput.value.trim();
-    var author = authorInput.value.trim();
-    var id = 1;
-    if(bookItems.length > 0){
-        id = bookItems[bookItems.length - 1].id + 1;
+    const title = titleInput.value.trim();
+    const author = authorInput.value.trim();
+    let id = 1;
+    if (bookItems.length > 0) {
+      id = bookItems[bookItems.length - 1].id + 1;
     }
-    if(!title || !author){
+    if (!title || !author) {
       return;
     }
     bookItems.push({
-      id: id,  
-      title: title,
-      author: author
+      id,
+      title,
+      author,
     });
-    saveToLocalStorage("bookItems", bookItems);
-  });  
+    saveToLocalStorage('bookItems', bookItems);
+    displayBooks();
+  });
