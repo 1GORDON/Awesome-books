e51a72912512537b9499858919a37afb5095f2a9
Gordon <gordon.tinyefuza@gmail.com>
2021-09-15 17:44:57 +0300

    fix eslint error

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ const saveToLocalStorage = (key, data) => localStorage.setItem(key, JSON.stringi
 const getFromLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
 
 function displayBooks(){
-  var bookItems = getFromLocalStorage("bookItems");
+  var bookItems = getFromLocalStorage('bookItems');
 
-  if(bookItems == null){
+  if (bookItems == null) {
   bookItems = [];
   }
 
   var booksCode = bookItems.map(book => codeForSingleBook(book));
-  document.getElementById("items").innerHTML = booksCode.join("");
+  document.getElementById('items').innerHTML = booksCode.join('');
 
-  var removeButtons = document.querySelectorAll(".remove");
-  for(let i=0; i<removeButtons.length; i++){
-      removeButtons[i].addEventListener("click", function(event){
-          let id = event.target.getAttribute("data-id");
+  var removeButtons = document.querySelectorAll('.remove');
+  for (let i=0; i<removeButtons.length; i++){
+      removeButtons[i].addEventListener('click', function(event){
+          let id = event.target.getAttribute('data-id');
           bookItems = bookItems.filter(b => b.id !== Number(id));
-          saveToLocalStorage("bookItems", bookItems);
+          saveToLocalStorage('bookItems', bookItems);
           displayBooks();
       });
   }
@@ -27,7 +27,7 @@ displayBooks();
 function codeForSingleBook(book){
     return `<div>${book.title}</div>
             <div>${book.author}</div>
-            <button class="remove" data-id="${book.id}">Remove</button>
+            <button class='remove' data-id='${book.id}'>Remove</button>
             <hr>`;
 }
 
@@ -55,6 +55,6 @@ document
       title,
       author,
     });
-    saveToLocalStorage("bookItems", bookItems);
+    saveToLocalStorage('bookItems', bookItems);
     displayBooks();
   });  
